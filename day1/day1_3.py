c6d7127cd2fa5ca9354e08bd253562f09ba7cd92
inha-kimhyunmin <kimhm396@inha.edu>
2025-02-11 15:09:47 +0900

    "v1.1"

--- day1/day1_3.py
+++ day1/day1_3.py
@@ -1,11 +1,18 @@
-# (100°F − 32) × 5/9 = 37.778°C
-# (0°C × 9/5) + 32 = 32°F
+# # (100°F − 32) × 5/9 = 37.778°C
+# # (0°C × 9/5) + 32 = 32°F
+#
+# menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Quit program : ")
+#
+# if menu == '1':
+#     fahrenheit = float(input('Input Fahrenheit : '))
+#     print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
+# elif menu == '2':
+#     celsius = float(input('Input Celcius : '))
+#     print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
 
-menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Quit program : ")
 
-if menu == '1':
-    fahrenheit = float(input('Input Fahrenheit : '))
-    print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
-elif menu == '2':
-    celsius = float(input('Input Celcius : '))
-    print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
+temp = {} #리스트가 비어있으면 false
+if temp:
+    print("True!!")
+else:
+    print("False!!")
