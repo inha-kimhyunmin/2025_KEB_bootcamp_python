36ce7e821125e89ac72327deaac588d7cdfe8cff
inha-kimhyunmin <kimhm396@inha.edu>
2025-02-11 15:41:48 +0900

    "v1.6"

--- day1/day1_3.py
+++ day1/day1_3.py
@@ -1,22 +1,6 @@
-course = "* KEB 2024# KEB !Bootcamp KEB...*!#"
-print(course.find('KEB')) #KEB 문자열의 시작위치
-print(course.rfind('KEB')) #reverse find?
-print(course.index('KEB'))
-print(course.rindex('KEB'))
-print(course.find('Inha'))  # -1 없으면 -1인가봄
-# print(course.index('Inha'))  # ValueError: substring not found 없으면 에러 발생
-
-print(course)
-course = course.replace('KEB', 'Inha', 2)
-print(course)
-print(course.strip()) # 문자열 앞뒤 공란 제거
-print(course.strip("!#.*")) #문자열 안에 문자들이 시작, 끝에 있으면 제거
-print(course.lstrip("!#.*")) #왼쪽만
-print(course.rstrip("!#.*")) #오른쪽만
-
-
-# print(course)
-# print(course.replace('KEB', 'Inha'))
-# print(course)
-# course = course.replace('KEB', 'Inha')
-# print(course)
+subjects = "python c++ database linux"
+subject = input("수강신청과목 입력 : ")
+if subjects.find(subject) != -1:
+    print(f"해당 과목이 존재합니다. 위치는 {subjects.find(subject)}번 인덱스입니다.")
+else:
+    print('해당과목이 존재하지 않습니다')
