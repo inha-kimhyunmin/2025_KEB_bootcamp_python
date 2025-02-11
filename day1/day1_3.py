d00bb8068d610af8a2d74e1a9e5a72562383d7e2
inha-kimhyunmin <kimhm396@inha.edu>
2025-02-11 15:26:14 +0900

    "v1.3-1"

--- day1/day1_3.py
+++ day1/day1_3.py
@@ -1,5 +1,19 @@
-l = [1, 3, 3, 2, 4]  # list
-s = {1, 3, 3, 2, 4}  # set
-print(l, s)  # [1, 3, 3, 2, 4] {1, 2, 3, 4}
+university = "Inha\nUniversity!"
+#university = r"Inha\nUniversity!"  # raw string 이거는 \n이 엔터키가 아닌 문자열 \n으로 나온다
+#print(university)
 
-#set은 중복 출력이 안된다
+#slicing \n은 문자 하나임, 역순 정렬하면 마지막은 -1로 함
+print(university[:4])
+print(university[:-11]) #뒤에서 11글자 빼고 가져옴
+print(len(university))
+print(university[0:len(university)])
+print(university[:16])
+print(university[::2]) #간격 2칸씩
+
+
+# number1 = input("First number : ")
+# number2 = input("Second number : ")
+# print(number1 + number2)  # concatenation
+# print(number1 * 3)  # duplicate
+# print(number1 + '3')  # TypeError: can only concatenate str (not "int") to str
+# print(number1 + 3)  # TypeError: can only concatenate str (not "int") to str
