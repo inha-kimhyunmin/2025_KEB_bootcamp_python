911820049e149eb5adbfc10f7470fbf021e1979a
inha-kimhyunmin <kimhm396@inha.edu>
2025-02-11 16:15:33 +0900

    "v1.8"

--- day1/day1_3.py
+++ day1/day1_3.py
@@ -1,11 +1,8 @@
-# preview
-subjects = "python c++ database linux"
-subject = input("수강신청과목 입력 : ")
-try:
-    print(f"해당 과목이 존재합니다. 위치는 {subjects.index(subject)}번 인덱스입니다.")
-except ValueError:
-    print('해당과목이 존재하지 않습니다')
+numbers = input("First Second ").split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
 
+if n1 > n2:
+    n1, n2 = n2, n1
 
-#index함수를 쓸 때 예외가 발생하면 except 구문으로 이동한다
-#index 함수에서는 ValueError 가 발생
+print(f"{n1} {n2}")
