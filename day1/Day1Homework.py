f9c9db4ee0c025634c4e66cdfbfcbe8a3653f3cb
inha-kimhyunmin <kimhm396@inha.edu>
2025-02-11 16:27:36 +0900

    "v1.3"

--- day1/Day1Homework.py
+++ day1/Day1Homework.py
@@ -1,7 +1,7 @@
 def is_prime(num) -> bool:
     if num >= 2:
         i = 2
-        while i <= int(num ** 0.5):
+        while i <= int(pow(num, 0.5)):
             if num % i == 0:
                 return False
             i = i + 1
@@ -18,7 +18,12 @@ n2 = int(number[1])
 if n1 > n2:
     n1, n2 = n2, n1
 
+cnt = 0
 while n1 <= n2:
-    if (is_prime(n1)):
+    if is_prime(n1):
         print(f"{n1} ", end = '')
+        cnt = cnt + 1
+    if cnt == 20:
+        cnt = 0
+        print()
     n1 = n1 + 1
