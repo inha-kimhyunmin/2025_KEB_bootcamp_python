a1d2d941474ccbfa695a59b1406646b3d7f41dc3
inha-kimhyunmin <kimhm396@inha.edu>
2025-02-11 15:15:29 +0900

    "v1.2"

--- day1/day1_3.py
+++ day1/day1_3.py
@@ -1,18 +1,21 @@
-# # (100°F − 32) × 5/9 = 37.778°C
-# # (0°C × 9/5) + 32 = 32°F
-#
-# menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Quit program : ")
-#
-# if menu == '1':
-#     fahrenheit = float(input('Input Fahrenheit : '))
-#     print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
-# elif menu == '2':
-#     celsius = float(input('Input Celcius : '))
-#     print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
+letter = input('Input alphabet letter : ')
+vowels = {'a', 'e', 'i', 'o', 'u', 'e'}  # set 중괄호를 사용하여 생성 -> 집합
+#set 도 immutable
 
+# ✅ 1. set의 특징
+# 중복을 허용하지 않는다.
+# 순서가 없다. (list와 달리 인덱싱 불가)
+# 빠른 검색 속도 (in 연산자가 O(1))
+# 변경 가능 (mutable) 하지만, 내부 요소는 변경 불가능해야 함 (immutable)
 
-temp = {} #리스트가 비어있으면 false
-if temp:
-    print("True!!")
+# vowels = "aeiuo"  # str
+print(vowels)
+print(type(vowels))
+if letter in vowels:  # in
+    print(f'{letter} is a vowel~')
 else:
-    print("False!!")
+    print(f'{letter} is a consonant!')
+
+
+#letter in vowels 는 vowels 문자열안에 letter가 있으면 참
+#무슨 이런 날먹이 다있어?
