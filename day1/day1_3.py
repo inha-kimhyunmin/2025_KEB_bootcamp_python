a958230a6b41165438858a6e987be718652f16ed
inha-kimhyunmin <kimhm396@inha.edu>
2025-02-11 15:16:14 +0900

    "v1.3"

--- day1/day1_3.py
+++ day1/day1_3.py
@@ -1,21 +1,5 @@
-letter = input('Input alphabet letter : ')
-vowels = {'a', 'e', 'i', 'o', 'u', 'e'}  # set 중괄호를 사용하여 생성 -> 집합
-#set 도 immutable
+l = [1, 3, 3, 2, 4]  # list
+s = {1, 3, 3, 2, 4}  # set
+print(l, s)  # [1, 3, 3, 2, 4] {1, 2, 3, 4}
 
-# ✅ 1. set의 특징
-# 중복을 허용하지 않는다.
-# 순서가 없다. (list와 달리 인덱싱 불가)
-# 빠른 검색 속도 (in 연산자가 O(1))
-# 변경 가능 (mutable) 하지만, 내부 요소는 변경 불가능해야 함 (immutable)
-
-# vowels = "aeiuo"  # str
-print(vowels)
-print(type(vowels))
-if letter in vowels:  # in
-    print(f'{letter} is a vowel~')
-else:
-    print(f'{letter} is a consonant!')
-
-
-#letter in vowels 는 vowels 문자열안에 letter가 있으면 참
-#무슨 이런 날먹이 다있어?
+#set은 중복 출력이 안된다
