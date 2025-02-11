7422c7b909629e00d7ff160d74496a67d0795646
inha-kimhyunmin <kimhm396@inha.edu>
2025-02-11 14:20:22 +0900

    v0.4

--- day1/day1_2.py
+++ day1/day1_2.py
@@ -1,6 +1,11 @@
-num = int(input("Input num : "))  #
 
-def isprime(n):
+def isprime(n) -> bool: #type hint 리턴타입 뭔지 알려줌 이걸 썼다고 해서 문법적 제약이 생기지는 않는다
+    """
+        소수 여부를 판정해서 소수면 True를 소수가 아니면 False를 리턴
+
+        :param n: 판정할 자연수(integer)
+        :return: boolean
+        """
     if n < 2:
         return False
     else:
@@ -11,6 +16,9 @@ def isprime(n):
 
 count = 0
 
+help(abs)
+help(isprime)
+num = int(input("Input num : "))  #
 for i in range(1, num, 1):
     if isprime(i):
         print(f"{i} ", end=' ')
---
+++ day1/day1_3.py
@@ -0,0 +1,7 @@
+
+
+artists = ['BTS', 'NJZ', '핑클', 'SES', 'HOT', '블랙핑크']
+groups = artists #주소만 복사
+print(artists, groups)
+artists[2] = '세븐틴'
+print(artists, groups)
