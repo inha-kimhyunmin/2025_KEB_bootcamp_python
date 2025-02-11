992e19c748775ee4506ce1bd015e1387c92a3556
inha-kimhyunmin <kimhm396@inha.edu>
2025-02-11 16:23:37 +0900

    "v1.2"

--- day1/Day1Homework.py
+++ day1/Day1Homework.py
@@ -1,7 +1,7 @@
 def is_prime(num) -> bool:
     if num >= 2:
         i = 2
-        while i <= int(num**0.5):
+        while i <= int(num ** 0.5):
             if num % i == 0:
                 return False
             i = i + 1
@@ -9,10 +9,16 @@ def is_prime(num) -> bool:
         return False
     return True
 
-number = int(input("Input Number : "))
 
-if(is_prime(number)):
-    print(f"{number} is Prime number")
-else:
-    print(f"{number} is Not Prime number")
+number = input("First number Second number : ").split()
 
+n1 = int(number[0])
+n2 = int(number[1])
+
+if n1 > n2:
+    n1, n2 = n2, n1
+
+while n1 <= n2:
+    if (is_prime(n1)):
+        print(f"{n1} ", end = '')
+    n1 = n1 + 1
