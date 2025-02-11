2414e718ff9a8c300518cb937e0841e55c980a03
inha-kimhyunmin <kimhm396@inha.edu>
2025-02-11 14:31:11 +0900

    "v0.6"

--- day1/day1_3.py
+++ day1/day1_3.py
@@ -1,9 +1,11 @@
+# base_number = int(input('Input base number : '))
+# exponent_number = int(input('Input exponent number : '))
+# print(f'밑은 {base_number}, 지수는{exponent_number}, 결과 값은 {base_number**exponent_number}')
 
-
-artists = ['BTS', 'NJZ', '핑클', 'SES', 'HOT', '블랙핑크']
-groups = artists #주소만 복사
-print(artists, groups)
-artists[2] = '세븐틴'
-print(artists, groups)
-
-# groups는 artists의 주소를 같이쓰고있기때문에 artists의 2번째 값이 바뀌면 groups도 같이 바뀐다.
+money = 5,000,000 # list로 첫 번째 원소 5, 두 번째 원소 0, 세 번째 원소 0
+print(money) #tuple의 원소가 정수로 인식해서 000이 아닌 0으로 인식
+# money[2] = 7 이건 안됨
+print(type(money))  # tuple
+cash = 5_000_000
+print(cash)
+print(type(cash))  # int
