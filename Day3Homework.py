381c522705f3a2e41b3cd084d97f35c9a863b6e6
inha-kimhyunmin <kimhm396@inha.edu>
2025-02-12 16:56:41 +0900

    v2.4 corresponding to menu deletetion, addition

---
+++ Day3Homework.py
@@ -0,0 +1,31 @@
+import random
+
+drink = ["위스키", "와인", "소주", "고량주"]
+snack = ["초콜릿", "치즈", "삼겹살", "양꼬치"]
+
+drink.append("사케")
+snack.append("광어회")
+
+
+def printchoice():
+    for i in range(0, len(drink)):
+        print(f"{i + 1}) {drink[i]} ", end='')
+    print(f"{len(drink) + 1}) 아무거나 {len(drink) + 2}) 종료 : ", end='')
+
+
+while True:
+    printchoice()
+    try:
+        menu = int(input())
+        if menu >= 1 and menu <= len(drink):
+            print(f"{drink[menu - 1]}에 어울리는 안주는 {snack[menu - 1]} 입니다")
+        elif menu == len(drink) + 1:
+            random_drink = random.randint(0, len(drink) - 1)
+            print(f'{drink[random_drink]}에 어울리는 안주는 {snack[random_drink]} 입니다')
+        elif menu == len(drink) + 2:
+            print(f'다음에 또 오세요')
+            break
+        else:
+            print("잘못된 입력입니다")
+    except:
+        print("잘못된 입력입니다")
