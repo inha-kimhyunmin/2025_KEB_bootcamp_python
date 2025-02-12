13c29b16fb61405847bfba13e4d3ae10a4298522
inha-kimhyunmin <kimhm396@inha.edu>
2025-02-12 14:01:59 +0900

    "v3.8 Day3"

--- day03.py
+++ day03.py
@@ -1,15 +1,17 @@
-# subjects = ["데이터베이스", "C++", "5", "Java", "Python", "Java", 9, "리눅스"]
-subjects = ["데이터베이스", "C++", "5", "Java", "Python", "Java", "9", "리눅스"]
+import copy
+subjects = ["a", "b", "c"]
+# subjects = ["a", ["b", "c"], "d"]
+a = subjects #주소 복사 -> 같은거 참조
+b = subjects.copy() #얕은 복사
+c = list(subjects) #subjects[:] 와 동일한 동작
+d = subjects[:] # 얕은 복사
+e = copy.deepcopy(a) #메모리 공간에 새롭게 할당 -> 원본과 아예 관련이 없음
+print(subjects, a, b, c, d, e)
+# subjects[1][1] = "x"
+subjects[1] ="x"
+print(subjects, a, b, c, d, e)
 
-print(subjects[::-1]) #첫번째는 시작위치, 두번째는 끝위치, 세번째는 간격
-print(subjects[-1:2:-1]) #reverse로 하면 맨 뒤가 -1
-
-subjects.sort(reverse=True) #desc 문자열과 숫자는 비교할 수 없다.
-print(subjects)
-
-print()
-
-copy_subjects = sorted(subjects)
-print(subjects)
-print(copy_subjects) #copy를 하면 원본은 손상되지 않는다
-
+#list의 원소가 불변 -> 리스트의 원소 자체를 수정은 못하고 새로운 값을 리스트에 할당
+#list의 원소가 가변 -> 리스트의 원소 자체를 수정을 할 수 있음(list, dict, set) 등등
+#얕은 복사를 할때 리스트의 원소가 수정 가능하면 복사를 한 리스트에서 원소를 수정하면 원본도 손상될 우려가 있다!!!!
+#deepcopy를 사용하면 참조하는 원소조차 새로 만들기 때문에 원본 손상의 우려가 없다. 그러나 메모리 공간을 많이 잡아먹는다.
