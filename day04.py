693bb060f4c55715e272f896f1dce2576a250408
inha-kimhyunmin <kimhm396@gmail.com>
2025-02-13 14:28:40 +0900

    "v5.2 decorator"

--- day04.py
+++ day04.py
@@ -1,5 +1,7 @@
 # SOLID
 # Open Closed Principle : 개방 폐쇄 원칙(확장에는 열려 있고 수정에는 닫혀있는 원칙)
+import time
+
 
 def factorial(n) -> int:
     """
@@ -12,12 +14,29 @@ def factorial(n) -> int:
     else:
         return 1
 
-def factorial_repetition(n) ->int:
+
+def time_decorator(func):
+    def wrapper(*arg):
+        s = time.time()
+        r = func(*arg)
+        e = time.time()
+        print(f"걸린 시간 : {e - s}s")
+        return r
+
+    return wrapper
+
+
+@time_decorator
+def factorial_repetition(n) -> int:
     result = 1
-    for i in range(2, n+1):
+    for i in range(2, n + 1):
         result = result * i
     return result
 
+
 number = int(input())
+s = time.time()
 print(f"{number}! = {factorial_repetition(number)}")
+e = time.time()
+# print(f"걸린 시간 : {e - s}")  # 40us 걸림
 print(factorial(5))
