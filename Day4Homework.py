aab64662d1e309dcc591cc53d3414bc8b3209c94
inha-kimhyunmin <kimhm396@gmail.com>
2025-02-13 16:52:10 +0900

    "v3.8 kwargs example"

--- Day4Homework.py
+++ Day4Homework.py
@@ -1,57 +1,23 @@
-# v3.6) 2중 데코레이터 적용, 성능측정 데코레이터, 디스크립션 테코레이터를 팩토리얼 함수에 적용시키시오
-import time
+# V3.8) kwargs를 사용한 데코레이터 예제
 
-
-def description(f):  # closure
-    def inner(*args):
-        print(f.__name__)
-        print(f.__doc__)
-        r = f(*args)
-        return r
-
-    return inner
-
-
-def time_decorator(func):
-    def wrapper(*arg):
-        s = time.time()
-        r = func(*arg)
-        e = time.time()
-        print(f"걸린 시간 : {e - s}s")
-        return r
+def log_decorator(func):
+    def wrapper(*args, **kwargs):
+        print(f"Function Name : {func.__name__}")
+        print(f"Arguments : {args}")
+        print(f"Keyword Arguments : {kwargs}")
+        result = func(*args, **kwargs)
+        return result
 
     return wrapper
 
 
-@description
-@time_decorator
-def factorial_repetition(n) -> int:
-    """
-    n!을 구하는 함수
-    :param n: int
-    :return: int
-    """
-    result = 1
-    for i in range(2, n + 1):
-        result = result * i
-    return result
+@log_decorator
+def greet(name, greeting="안녕하세요", age=0):
+    return f"{greeting}, {name}"
 
 
-def factorial(n) -> int:
-    """
-    n!을 구하는 함수
-    :param n: int
-    :return: int
-    """
-    if n > 1:
-        return n * factorial(n - 1)
-    else:
-        return 1
-
-
-number = int(input())
-print(f"{number}! = {factorial_repetition(number)}")
-
-# print(f"{number}! = {factorial_repetition(number)}")
-# print(f"걸린 시간 : {e - s}")  # 40us 걸림
-# print(factorial(5))
+print(greet("인하"))
+print(greet("James", "Hello"))
+print(greet("James", "Hello", 10))
+print(greet("James", greeting="Nihao",
+            age=20))  # 이렇게 하면 james는 튜플(가변인자)로 받고, 뒤는 key-value값 형태로 전달해서 keyword argument로 넘겨진다.
