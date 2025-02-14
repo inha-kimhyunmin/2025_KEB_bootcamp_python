ad436b2fc30866918efa0a422f6220e72036fcd8
inha-kimhyunmin <kimhm396@gmail.com>
2025-02-14 11:22:43 +0900

    "v5.8 bomb countdown with recursion"

--- day05.py
+++ day05.py
@@ -1,74 +1,33 @@
 import time
 
 
-def factorial_repetition(n) -> int:
-    '''
-    반복문을 이용한 팩토리얼 함수
-    :param n: 정수, int
-    :return: 팩토리얼 값, int
-    '''
-    result = 1
-    for i in range(2, n + 1):
-        result = result * i
-    return result
-
-
-def factorial_recursion(n):
-    '''
-    재귀함수를 사용한 팩토리얼 함수
-    :param n: 정수, int
-    :return: function, int
-    '''
-    if n == 1:
-        return n
+def time_bomb(n):
+    """
+    n초 뒤에 터지는 시한폭탄!!!
+    :param n: int
+    :return: recursion
+    """
+    if n >= 0:
+        print(f"{n}")
+        time.sleep(1)
+        return time_bomb(n - 1)
     else:
-        return n * factorial_recursion(n - 1)
+        print("BOOM!!")
 
 
-def fibonacci(n):
+def time_bomb_repetition(n):
     """
-    n번째 피보나치 수를 구함 1번째는 0, 2번째는 1
+    n초 뒤에 터지는 시한폭탄!!
     :param n: int
-    :return: int
+    :return: x
     """
-    if n > 2:
-        return fibonacci(n - 1) + fibonacci(n - 2)
-    elif n == 2:
-        return 1
-    elif n == 1:
-        return 0
+    sec = n
+    while (sec >= 0):
+       print(f"{sec}")
+       sec = sec - 1
+       time.sleep(1)
+    print("BOOM!!")
 
 
-def fibonacci_repetition(n):
-    """
-    n 번째 피보나치 수를 구함 1번째는 0, 2번째는 1
-    반복문을 이용해서 구현(리스트 사용)
-    :param n: int
-    :return: int
-    """
-    fibo = [0, 1]
-    if (n > 2):
-        for i in range(2, n):
-            fibo.append(fibo[i - 1] + fibo[i - 2])
-        return fibo[n - 1]
-    elif n == 2:
-        return 1
-    elif n == 1:
-        return 0
-
-
-num = int(input("Input Number : "))
-s = time.time()
-print(fibonacci(num))  # 파이썬은 이게 이렇게 오래걸려?
-e = time.time()
-print(f"걸린 시간 : {e - s}s")
-
-s = time.time()
-print(fibonacci_repetition(num))  # 파이썬은 이게 이렇게 오래걸려?
-e = time.time()
-print(f"걸린 시간 : {e - s}s")
-
-# number = int(input("number : "))
-# print(factorial_repetition(number))
-# print(factorial_recursion(number))
-# print(globals())
+time_bomb_repetition(5)
+time_bomb(3)
