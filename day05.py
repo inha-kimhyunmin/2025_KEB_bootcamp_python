1908e8ae37aa23cb826ed5d253ec332a875ade7e
inha-kimhyunmin <kimhm396@gmail.com>
2025-02-17 17:02:54 +0900

    "v6.4 file open csv"

--- day05.py
+++ day05.py
@@ -1,12 +1,10 @@
 import random
+import csv
 
 try:
-    file = input("File name : ")
-    fp = open(file, 'r')  # 파일이름, 모드(읽기, 쓰기)
-    readme_list = fp.readlines()
-    rls = readme_list[0].split('_') #list의 0번째 인덱스에 있는 문자열을 _을 기준으로 나눔
-    print(readme_list)
-    print(rls)
-    fp.close()
+    with open("students.csv", 'r') as fp:
+        students_list = fp.readlines()
+        print(students_list)
+
 except FileNotFoundError as err:
-    print(f"{file} is not exist. {err}")
+    print(f"{err}")
